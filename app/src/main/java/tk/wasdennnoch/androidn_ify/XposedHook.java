0552172dd24dd84bb670d5b16c739a33c44831ff
wasdennnoch <leiter04@gmail.com>
2016-04-01 16:27:31 +0200

    Moved hooking code where it belongs, started implementing new notification design

--- app/src/main/java/tk/wasdennnoch/androidn_ify/XposedHook.java
+++ app/src/main/java/tk/wasdennnoch/androidn_ify/XposedHook.java
@@ -1,23 +1,31 @@
 package tk.wasdennnoch.androidn_ify;
 
+import android.content.res.XResources;
+import android.util.TypedValue;
+import android.widget.LinearLayout;
+
+import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
+import de.robv.android.xposed.callbacks.XC_LayoutInflated;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import tk.wasdennnoch.androidn_ify.recents.doubletap.DoubleTapHwKeys;
 import tk.wasdennnoch.androidn_ify.recents.doubletap.DoubleTapSwKeys;
 import tk.wasdennnoch.androidn_ify.settings.SettingsHooks;
 
-public class XposedHook implements IXposedHookLoadPackage, IXposedHookZygoteInit {
+public class XposedHook implements IXposedHookLoadPackage, IXposedHookZygoteInit, IXposedHookInitPackageResources {
 
     private static final String TAG = "[Android N-ify]";
     public static final String PACKAGE_ANDROID = "android";
     public static final String PACKAGE_SYSTEMUI = "com.android.systemui";
     public static final String PACKAGE_SETTINGS = "com.android.settings";
     public static boolean debug = true;
+    //private static String sModulePath;
 
-    private static XSharedPreferences sPrefs;
+    private static XSharedPreferences sPrefs = new XSharedPreferences("tk.wasdennnoch.androidn_ify");
 
     public static void logE(String tag, String msg, Throwable t) {
         XposedBridge.log(TAG + " [ERROR] " + tag + ": " + msg);
@@ -37,15 +45,10 @@ public class XposedHook implements IXposedHookLoadPackage, IXposedHookZygoteInit
         if (debug) XposedBridge.log(TAG + " [DEBUG] " + tag + ": " + msg);
     }
 
-    public static XSharedPreferences getPrefs() {
-        if (sPrefs == null)
-            sPrefs = new XSharedPreferences("tk.wasdennnoch.androidn_ify");
-        return sPrefs;
-    }
-
     @Override
     public void initZygote(StartupParam startupParam) throws Throwable {
-        debug = getPrefs().getBoolean("debug_log", false);
+        //sModulePath = startupParam.modulePath;
+        debug = sPrefs.getBoolean("debug_log", false);
     }
 
     @Override
@@ -53,14 +56,52 @@ public class XposedHook implements IXposedHookLoadPackage, IXposedHookZygoteInit
 
         switch (lpparam.packageName) {
             case PACKAGE_SETTINGS:
-                SettingsHooks.hook(lpparam.classLoader, getPrefs());
+                SettingsHooks.hook(lpparam.classLoader, sPrefs);
                 break;
             case PACKAGE_SYSTEMUI:
-                DoubleTapSwKeys.hook(lpparam.classLoader, getPrefs());
+                DoubleTapSwKeys.hook(lpparam.classLoader, sPrefs);
                 break;
             case PACKAGE_ANDROID:
-                DoubleTapHwKeys.hook(lpparam.classLoader, getPrefs());
+                DoubleTapHwKeys.hook(lpparam.classLoader, sPrefs);
                 break;
+        }
+
+    }
+
+    @Override
+    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) throws Throwable {
+
+        if (sPrefs.getBoolean("enable_notification_tweaks", true)) {
+            XResources.DimensionReplacement zero = new XResources.DimensionReplacement(0, TypedValue.COMPLEX_UNIT_DIP);
+            if (resparam.packageName.equals(PACKAGE_SYSTEMUI)) {
+                resparam.res.setReplacement(PACKAGE_SYSTEMUI, "dimen", "qs_peek_height", zero);
+                resparam.res.setReplacement(PACKAGE_SYSTEMUI, "dimen", "notification_side_padding", zero);
+                resparam.res.setReplacement(PACKAGE_SYSTEMUI, "dimen", "notifications_top_padding", zero);
+                resparam.res.setReplacement(PACKAGE_SYSTEMUI, "dimen", "notification_padding", zero);
+                resparam.res.setReplacement(PACKAGE_SYSTEMUI, "dimen", "notification_material_rounded_rect_radius", zero);
+
+                //resparam.res.setReplacement(PACKAGE_SYSTEMUI, "dimen", "notification_material_rounded_rect_radius_negative", zero);
+                //resparam.res.setReplacement(PACKAGE_SYSTEMUI, "dimen", "notification_collapse_second_card_padding", new XResources.DimensionReplacement(-2, TypedValue.COMPLEX_UNIT_DIP)); // WRAP_CONTENT
+                //resparam.res.setReplacement(PACKAGE_SYSTEMUI, "dimen", "notification_children_padding", zero);
+                resparam.res.setReplacement(PACKAGE_SYSTEMUI, "dimen", "notification_children_divider_height", new XResources.DimensionReplacement(50, TypedValue.COMPLEX_UNIT_DIP));
+
+                //resparam.res.setReplacement(PACKAGE_SYSTEMUI, "dimen", "header_notifications_collide_distance", new XResources.DimensionReplacement(150, TypedValue.COMPLEX_UNIT_DIP)); // Testing
+
+                resparam.res.hookLayout("com.android.systemui", "layout", "status_bar_expanded_header", new XC_LayoutInflated() {
+                    @Override
+                    public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
+                        liparam.view.setPadding(0, 0, 0, 0);
+                    }
+                });
+                resparam.res.hookLayout("com.android.systemui", "layout", "qs_panel", new XC_LayoutInflated() {
+                    @Override
+                    public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
+                        ((LinearLayout.LayoutParams) liparam.view.getLayoutParams()).setMarginStart(0);
+                        ((LinearLayout.LayoutParams) liparam.view.getLayoutParams()).setMarginEnd(0);
+                    }
+                });
+            }
+
         }
 
     }
--- app/src/main/java/tk/wasdennnoch/androidn_ify/recents/doubletap/DoubleTapSwKeys.java
+++ app/src/main/java/tk/wasdennnoch/androidn_ify/recents/doubletap/DoubleTapSwKeys.java
@@ -14,8 +14,11 @@ public class DoubleTapSwKeys extends DoubleTapBase {
     private static final String TAG = "DoubleTapSwKeys";
 
     private static final String CLASS_PHONE_STATUS_BAR = "com.android.systemui.statusbar.phone.PhoneStatusBar";
+    private static final String CLASS_NAVIGATION_BAR_VIEW = "com.android.systemui.statusbar.phone.NavigationBarView";
 
-    private static boolean sOnClickHooked = false;
+    private static Context mContext;
+    private static Handler mHandler;
+    private static Object mNavigationBarView;
     private static boolean sWasPressed = false;
     private static View.OnClickListener sOriginalRecentsClickListener;
     private static View sRecentsButton;
@@ -31,74 +34,69 @@ public class DoubleTapSwKeys extends DoubleTapBase {
     private static XC_MethodHook prepareNavigationBarViewHook = new XC_MethodHook() {
         @Override
         protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-            final Context mContext = (Context) XposedHelpers.getObjectField(param.thisObject, "mContext");
+            XposedHook.logD(TAG, "prepareNavigationBarViewHook called");
+
+            mContext = (Context) XposedHelpers.getObjectField(param.thisObject, "mContext");
+            mHandler = (Handler) XposedHelpers.getObjectField(param.thisObject, "mHandler");
+            sOriginalRecentsClickListener = (View.OnClickListener) XposedHelpers.getObjectField(param.thisObject, "mRecentsClickListener");
+            mNavigationBarView = XposedHelpers.getObjectField(param.thisObject, "mNavigationBarView");
+            sRecentsButton = (View) XposedHelpers.callMethod(mNavigationBarView, "getRecentsButton");
 
             registerReceiver(mContext);
-
-            if (!isTaskLocked(mContext)) {
-                final Handler mHandler = (Handler) XposedHelpers.getObjectField(param.thisObject, "mHandler");
-                sOriginalRecentsClickListener = (View.OnClickListener) XposedHelpers.getObjectField(param.thisObject, "mRecentsClickListener");
-                final Object mNavigationBarView = XposedHelpers.getObjectField(param.thisObject, "mNavigationBarView");
-                sRecentsButton = (View) XposedHelpers.callMethod(mNavigationBarView, "getRecentsButton");
-
-                if (!sOnClickHooked) {
-                    // I'm basically hooking every view in the SystemUI here... but hey, it's working.
-                    //                             |      KeyButtonView      /   ImageView   /     View     |
-                    XposedHelpers.findAndHookMethod(sRecentsButton.getClass().getSuperclass().getSuperclass(), "setOnClickListener", View.OnClickListener.class, new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            final View.OnClickListener original = (View.OnClickListener) param.args[0];
-                            param.args[0] = new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    if (v != sRecentsButton) {
-                                        XposedHook.logD(TAG, "Other button clicked");
-                                        original.onClick(v);
-                                        return;
-                                    }
-                                    XposedHook.logD(TAG, "SW recents clicked");
-                                    if (!sWasPressed) {
-                                        sWasPressed = true;
-                                        mHandler.postDelayed(sResetPressedStateRunnable, mDoubletapSpeed);
-                                    } else {
-                                        XposedHook.logD(TAG, "Double tap detected");
-                                        mHandler.removeCallbacks(sResetPressedStateRunnable);
-                                        sWasPressed = false;
-                                        switchToLastApp(mContext, mHandler);
-                                    }
-                                }
-                            };
-                        }
-                    });
-                    XposedHelpers.findAndHookMethod(mNavigationBarView.getClass(), "reorient", new XC_MethodHook() {
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            XposedHook.logD(TAG, "Updating sRecentsButton in reorient()");
-                            sRecentsButton = (View) XposedHelpers.callMethod(mNavigationBarView, "getRecentsButton");
-                        }
-                    });
-                    sOnClickHooked = true;
+        }
+    };
+    private static XC_MethodHook setOnClickListenerHook = new XC_MethodHook() {
+        @Override
+        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+            final View.OnClickListener original = (View.OnClickListener) param.args[0];
+            param.args[0] = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (v != sRecentsButton) {
+                        XposedHook.logD(TAG, "Other button clicked");
+                        original.onClick(v);
+                        return;
+                    }
+                    XposedHook.logD(TAG, "SW recents clicked");
+                    if (!sWasPressed) {
+                        sWasPressed = true;
+                        mHandler.postDelayed(sResetPressedStateRunnable, mDoubletapSpeed);
+                    } else {
+                        XposedHook.logD(TAG, "Double tap detected");
+                        mHandler.removeCallbacks(sResetPressedStateRunnable);
+                        sWasPressed = false;
+                        if (!isTaskLocked(mContext))
+                            switchToLastApp(mContext, mHandler);
+                    }
                 }
+            };
+        }
+    };
+    private static XC_MethodHook reorientHook = new XC_MethodHook() {
+        @Override
+        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+            if (mNavigationBarView != null) {
+                XposedHook.logD(TAG, "Updating sRecentsButton in reorient()");
+                sRecentsButton = (View) XposedHelpers.callMethod(mNavigationBarView, "getRecentsButton");
+            } else {
+                XposedHook.logD(TAG, "Skipped updating sRecentsButton in reorient() because mNavigationBarView is null");
             }
         }
     };
 
     public static void hook(ClassLoader classLoader, XSharedPreferences prefs) {
-
         try {
-
             prefs.reload();
             loadPrefDoubleTapSpeed(prefs);
             if (prefs.getBoolean("enable_recents_tweaks", true)) {
-
-                Class<?> classPhoneStatusBar = XposedHelpers.findClass(CLASS_PHONE_STATUS_BAR, classLoader);
-                XposedHelpers.findAndHookMethod(classPhoneStatusBar, "prepareNavigationBarView", prepareNavigationBarViewHook);
+                XposedHelpers.findAndHookMethod(CLASS_PHONE_STATUS_BAR, classLoader, "prepareNavigationBarView", prepareNavigationBarViewHook);
+                // I'm hooking every view in the SystemUI here... but hey, it's working.
+                XposedHelpers.findAndHookMethod(View.class, "setOnClickListener", View.OnClickListener.class, setOnClickListenerHook);
+                XposedHelpers.findAndHookMethod(CLASS_NAVIGATION_BAR_VIEW, classLoader, "reorient", reorientHook);
             }
-
         } catch (Throwable t) {
             XposedHook.logE(TAG, "Error in hook", t);
         }
-
     }
 
 }
