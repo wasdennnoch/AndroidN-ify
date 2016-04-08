b8aaa4f627517a8f8a0b19914d1d8aa878a9e7b6
Viperus <viperusgaming@gmail.com>
2016-04-08 16:47:45 -0400

    Revert "manual update."
    This reverts commit daa642a1ea5ce31eb6f488ddcc645002d40a3fcb.

--- app/src/main/java/tk/wasdennnoch/androidn_ify/XposedHook.java
+++ app/src/main/java/tk/wasdennnoch/androidn_ify/XposedHook.java
@@ -1,6 +1,5 @@
 package tk.wasdennnoch.androidn_ify;
 
-import android.content.res.XModuleResources;
 import android.content.res.XResources;
 import android.util.TypedValue;
 import android.widget.LinearLayout;
@@ -20,37 +19,37 @@ import tk.wasdennnoch.androidn_ify.statusbar.header.StatusBarHeaderHooks;
 
 public class XposedHook implements IXposedHookLoadPackage, IXposedHookZygoteInit, IXposedHookInitPackageResources {
 
-    private static final String LOG_FORMAT = "[Android N-ify] %1$s %2$s: %3$s";
+    private static final String TAG = "[Android N-ify]";
     public static final String PACKAGE_ANDROID = "android";
     public static final String PACKAGE_SYSTEMUI = "com.android.systemui";
     public static final String PACKAGE_SETTINGS = "com.android.settings";
     public static boolean debug = true;
-    private static String sModulePath;
+    //private static String sModulePath;
 
-    private static XSharedPreferences sPrefs = new XSharedPreferences(XposedHook.class.getPackage().getName());
+    private static XSharedPreferences sPrefs = new XSharedPreferences("tk.wasdennnoch.androidn_ify");
 
     public static void logE(String tag, String msg, Throwable t) {
-        XposedBridge.log(String.format(LOG_FORMAT, "[ERROR]", tag, msg));
+        XposedBridge.log(TAG + " [ERROR] " + tag + ": " + msg);
         if (t != null)
             XposedBridge.log(t);
     }
 
     public static void logW(String tag, String msg) {
-        XposedBridge.log(String.format(LOG_FORMAT, "[WARNING]", tag, msg));
+        XposedBridge.log(TAG + " [WARNING] " + tag + ": " + msg);
     }
 
     @SuppressWarnings("unused")
-    public static void logI(String tag, String msg) {
-        XposedBridge.log(String.format(LOG_FORMAT, "[INFO]", tag, msg));
+    public static void log(String tag, String msg) {
+        XposedBridge.log(TAG + " " + tag + ": " + msg);
     }
 
     public static void logD(String tag, String msg) {
-        if (debug) XposedBridge.log(String.format(LOG_FORMAT, "[DEBUG]", tag, msg));
+        if (debug) XposedBridge.log(TAG + " [DEBUG] " + tag + ": " + msg);
     }
 
     @Override
     public void initZygote(StartupParam startupParam) throws Throwable {
-        sModulePath = startupParam.modulePath;
+        //sModulePath = startupParam.modulePath;
         debug = sPrefs.getBoolean("debug_log", false);
     }
 
@@ -77,9 +76,6 @@ public class XposedHook implements IXposedHookLoadPackage, IXposedHookZygoteInit
 
         if (resparam.packageName.equals(PACKAGE_SYSTEMUI)) {
             if (sPrefs.getBoolean("enable_notification_tweaks", true)) {
-
-                XModuleResources modRes = XModuleResources.createInstance(sModulePath, resparam.res);
-
                 XResources.DimensionReplacement zero = new XResources.DimensionReplacement(0, TypedValue.COMPLEX_UNIT_DIP);
 
                 // Notifications
@@ -102,28 +98,18 @@ public class XposedHook implements IXposedHookLoadPackage, IXposedHookZygoteInit
                 resparam.res.hookLayout("com.android.systemui", "layout", "status_bar_expanded_header", new XC_LayoutInflated() {
                     @Override
                     public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
-                        liparam.view.setElevation(0);
                         liparam.view.setPadding(0, 0, 0, 0);
+                        liparam.view.setElevation(0);
                     }
                 });
                 resparam.res.hookLayout("com.android.systemui", "layout", "qs_panel", new XC_LayoutInflated() {
                     @Override
                     public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
-                        liparam.view.setElevation(0);
                         LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) liparam.view.getLayoutParams();
                         params.setMarginStart(0);
                         params.setMarginEnd(0);
                     }
                 });
-
-                // Colors
-                resparam.res.setReplacement(PACKAGE_SYSTEMUI, "color", "qs_tile_divider", 0x00FFFFFF);
-
-                // Drawables
-                resparam.res.setReplacement(PACKAGE_SYSTEMUI, "drawable", "notification_header_bg", modRes.fwd(R.drawable.replacement_notification_header_bg));
-                resparam.res.setReplacement(PACKAGE_SYSTEMUI, "drawable", "notification_material_bg", modRes.fwd(R.drawable.replacement_notification_material_bg));
-                resparam.res.setReplacement(PACKAGE_SYSTEMUI, "drawable", "notification_material_bg_dim", modRes.fwd(R.drawable.replacement_notification_material_bg_dim));
-
             }
 
         }
@@ -131,4 +117,3 @@ public class XposedHook implements IXposedHookLoadPackage, IXposedHookZygoteInit
     }
 
 }
-
