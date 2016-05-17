eeec99d445ef71d1258a11b823e4d68a49c36473
wasdennnoch <leiter04@gmail.com>
2016-05-17 18:26:34 +0200

    Support for Settings Editor

--- app/src/main/java/tk/wasdennnoch/androidn_ify/settings/SettingsHooks.java
+++ app/src/main/java/tk/wasdennnoch/androidn_ify/settings/SettingsHooks.java
@@ -1,12 +1,5 @@
 package tk.wasdennnoch.androidn_ify.settings;
 
-import android.content.Context;
-import android.os.Build;
-
-import java.util.List;
-
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedHelpers;
 import tk.wasdennnoch.androidn_ify.XposedHook;
 import tk.wasdennnoch.androidn_ify.settings.summaries.SummaryTweaks;
 import tk.wasdennnoch.androidn_ify.utils.ConfigUtils;
@@ -21,12 +14,6 @@ public class SettingsHooks {
             SummaryTweaks.afterOnCreate(param);
         }
     };*/
-    private static XC_MethodHook loadCategoriesFromResourceHook = new XC_MethodHook() {
-        @Override
-        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-            SummaryTweaks.afterLoadCategoriesFromResource(param);
-        }
-    };
 
     public static void hook(ClassLoader classLoader) {
         try {
@@ -34,14 +21,7 @@ public class SettingsHooks {
             config.reload();
             if (config.settings.enable_summaries) {
 
-                Class<?> classSettingsActivity = XposedHelpers.findClass("com.android.settings.SettingsActivity", classLoader);
-
-                //XposedHelpers.findAndHookMethod(classSettingsActivity, "onCreate", Bundle.class, onCreateHook);
-
-                if (Build.VERSION.SDK_INT >= 23)
-                    XposedHelpers.findAndHookMethod(classSettingsActivity, "loadCategoriesFromResource", int.class, List.class, Context.class, loadCategoriesFromResourceHook);
-                else
-                    XposedHelpers.findAndHookMethod(classSettingsActivity, "loadCategoriesFromResource", int.class, List.class, loadCategoriesFromResourceHook);
+                SummaryTweaks.hookMethods(classLoader);
 
                 // TODO performance testing
                 /*XposedHelpers.findAndHookMethod(SettingsActivity, "getDashboardCategories", boolean.class, new XC_MethodHook() {
