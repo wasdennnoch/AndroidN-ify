3f8bcb8da74c4ac4e14f4ae186249fcab06ef18f
wasdennnoch <leiter04@gmail.com>
2016-05-22 14:57:32 +0200

    Option to force english language

--- app/src/main/java/tk/wasdennnoch/androidn_ify/utils/ThemeUtils.java
+++ app/src/main/java/tk/wasdennnoch/androidn_ify/utils/ThemeUtils.java
@@ -4,14 +4,17 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+
+import java.util.Locale;
 
 import tk.wasdennnoch.androidn_ify.R;
 
 public class ThemeUtils {
 
-    @SuppressWarnings("ConstantConditions")
+    @SuppressWarnings({"ConstantConditions", "deprecation"})
     public static void applyTheme(Activity activity, SharedPreferences prefs) {
         if (prefs.getBoolean("app_dark_theme", false))
             activity.setTheme(R.style.DarkTheme);
@@ -22,9 +25,14 @@ public class ThemeUtils {
         int colorPrimaryDark = Color.HSVToColor(hsv);
         activity.getWindow().setStatusBarColor(colorPrimaryDark);
         activity.getActionBar().setBackgroundDrawable(new ColorDrawable(colorPrimary));
+        Locale locale = prefs.getBoolean("force_english", false) ? Locale.ENGLISH : Locale.getDefault();
+        //Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        activity.getResources().updateConfiguration(config, null);
     }
 
-    @SuppressWarnings("ConstantConditions")
+    @SuppressWarnings({"ConstantConditions", "deprecation"})
     public static void applyTheme(Dialog dialog, Context context, SharedPreferences prefs) {
         int colorPrimary = prefs.getInt("theme_colorPrimary", context.getResources().getColor(R.color.colorPrimary));
         float[] hsv = new float[3];
