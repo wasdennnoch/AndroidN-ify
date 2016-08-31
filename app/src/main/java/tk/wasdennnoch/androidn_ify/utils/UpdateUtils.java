b282f3a41e854cce7e61bfaf52080d51e6b5d520
paphonbth <paphonb@icloud.com>
2016-08-31 21:56:58 +0700

    Show changelog in update notification

--- app/src/main/java/tk/wasdennnoch/androidn_ify/utils/UpdateUtils.java
+++ app/src/main/java/tk/wasdennnoch/androidn_ify/utils/UpdateUtils.java
@@ -19,6 +19,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import tk.wasdennnoch.androidn_ify.R;
 import tk.wasdennnoch.androidn_ify.ui.misc.DownloadService;
@@ -59,13 +61,25 @@ public class UpdateUtils {
                 context.getString(R.string.update_notification_download), intent)
                 .build();
 
+        String content = String.format(context.getString(R.string.update_notification), updateData.getNumber());
+
         Notification.Builder notificationBuider = new Notification.Builder(context)
                 .setSmallIcon(R.drawable.ic_stat_n)
                 .setContentTitle(context.getString(R.string.update_notification_title))
-                .setContentText(String.format(context.getString(R.string.update_notification), updateData.getNumber()))
+                .setContentText(content)
                 .setColor(context.getResources().getColor(R.color.colorAccent));
         notificationBuider.setPriority(Notification.PRIORITY_HIGH).setVibrate(new long[0]);
         notificationBuider.addAction(downloadAction);
+
+        List<String> changes = updateData.getChanges();
+        if (changes.size() != 0) {
+            Notification.InboxStyle style = new Notification.InboxStyle();
+            style.addLine(content);
+            for (String change : changes) {
+                style.addLine(" - " + change);
+            }
+            notificationBuider.setStyle(style);
+        }
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(0, notificationBuider.build());
@@ -127,17 +141,20 @@ public class UpdateUtils {
         private int number;
         private boolean hasArtifact;
         private String artifactUrl;
+        private final List<String> changes;
 
-        public UpdateData(int number, boolean hasArtifact, String artifactUrl) {
+        public UpdateData(int number, boolean hasArtifact, String artifactUrl, List<String> changes) {
             this.number = number;
             this.hasArtifact = hasArtifact;
             this.artifactUrl = artifactUrl;
+            this.changes = changes;
         }
 
         public static UpdateData fromJson(JSONObject jsonObject) throws JSONException {
             int number = jsonObject.getInt("number");
             String url = jsonObject.getString("url");
             String artifactUrl = "";
+            List<String> changes = new ArrayList<>();
             boolean hasArtifact = false;
             JSONArray artifacts = jsonObject.getJSONArray("artifacts");
             int artifactCount = artifacts.length();
@@ -146,7 +163,17 @@ public class UpdateUtils {
                 JSONObject artifact = artifacts.getJSONObject(0);
                 artifactUrl = url + "artifact/" + artifact.getString("relativePath");
             }
-            return new UpdateData(number, hasArtifact, artifactUrl);
+            JSONObject changeSet = jsonObject.getJSONObject("changeSet");
+            if (changeSet != null) {
+                JSONArray items = changeSet.getJSONArray("items");
+                if (items != null) {
+                    for (int i = 0; i < items.length(); i++) {
+                        JSONObject item = items.getJSONObject(i);
+                        changes.add(item.getString("msg"));
+                    }
+                }
+            }
+            return new UpdateData(number, hasArtifact, artifactUrl, changes);
         }
 
         public String getArtifactUrl() {
@@ -160,5 +187,7 @@ public class UpdateUtils {
         public int getNumber() {
             return number;
         }
+
+        public List<String> getChanges() { return changes; }
     }
 }
