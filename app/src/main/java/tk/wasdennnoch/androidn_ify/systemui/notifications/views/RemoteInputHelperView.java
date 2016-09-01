aff944a7396ea830fa6f063f1ac00f1525b2806d
paphonbth <paphonb@icloud.com>
2016-09-01 21:39:26 +0700

    Direct reply improvements
    - Direct reply circular reveal animation
    - Prevent sending reply if edittext is blank
    - Increase actiosn buttons height to match N and direct reply

--- app/src/main/java/tk/wasdennnoch/androidn_ify/systemui/notifications/views/RemoteInputHelperView.java
+++ app/src/main/java/tk/wasdennnoch/androidn_ify/systemui/notifications/views/RemoteInputHelperView.java
@@ -5,18 +5,24 @@ import android.app.RemoteInput;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import tk.wasdennnoch.androidn_ify.R;
 import tk.wasdennnoch.androidn_ify.systemui.notifications.NotificationHooks;
@@ -28,7 +34,7 @@ import static de.robv.android.xposed.XposedHelpers.callStaticMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
 import static de.robv.android.xposed.XposedHelpers.getObjectField;
 
-public class RemoteInputHelperView extends FrameLayout implements View.OnClickListener, View.OnFocusChangeListener {
+public class RemoteInputHelperView extends FrameLayout implements View.OnClickListener, View.OnFocusChangeListener, TextView.OnEditorActionListener, TextWatcher {
 
     public static boolean DIRECT_REPLY_ENABLED = false;
 
@@ -51,7 +57,10 @@ public class RemoteInputHelperView extends FrameLayout implements View.OnClickLi
         sendButton = (ImageButton) findViewById(android.R.id.button1);
         sendProgress = (ProgressBar) findViewById(android.R.id.progress);
 
+        remoteInputText.setSingleLine(true);
         remoteInputText.setOnFocusChangeListener(this);
+        remoteInputText.setOnEditorActionListener(this);
+        remoteInputText.addTextChangedListener(this);
         remoteInputText.setOnClickListener(this);
         sendButton.setOnClickListener(this);
     }
@@ -69,8 +78,8 @@ public class RemoteInputHelperView extends FrameLayout implements View.OnClickLi
         remoteInputText.setHint(remoteInput.getLabel());
     }
 
-    public void show() {
-        remoteInputView.setVisibility(VISIBLE);
+    public void show(View view, boolean animate) {
+        showRemoteInput(view, animate);
         setWindowManagerFocus(true);
         remoteInputText.setFocusableInTouchMode(true);
         remoteInputText.setFocusable(true);
@@ -100,6 +109,20 @@ public class RemoteInputHelperView extends FrameLayout implements View.OnClickLi
         } catch (Throwable t) {
             log(t);
         }
+    }
+
+    private void showRemoteInput(View view, boolean animate) {
+        remoteInputView.setVisibility(VISIBLE);
+        if (!animate) return;
+        int cx = view.getLeft() + view.getWidth() / 2;
+        int cy = view.getTop() + view.getHeight() / 2;
+        int w = remoteInputView.getWidth();
+        int h = remoteInputView.getHeight();
+        int r = Math.max(
+                Math.max(cx + cy, cx + (h - cy)),
+                Math.max((w - cx) + cy, (w - cx) + (h - cy)));
+        ViewAnimationUtils.createCircularReveal(remoteInputView, cx, cy, 0, r)
+                .start();
     }
 
     public boolean requestScrollTo() {
@@ -137,23 +160,29 @@ public class RemoteInputHelperView extends FrameLayout implements View.OnClickLi
     @Override
     public void onClick(View v) {
         if (v == sendButton) {
-            setWindowManagerFocus(false);
-            remoteInputText.setFocusable(false);
-            remoteInputText.setEnabled(false);
-            sendButton.setVisibility(INVISIBLE);
-            sendProgress.setVisibility(VISIBLE);
-
-            Bundle resultData = new Bundle();
-            resultData.putString(remoteInput.getResultKey(), remoteInputText.getText().toString());
-            Intent result = new Intent().addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            RemoteInput.addResultsToIntent(remoteInputs, result, resultData);
-            try {
-                pendingIntent.send(getContext(), 0, result);
-            } catch (PendingIntent.CanceledException e) {
-                e.printStackTrace();
-            }
+            sendRemoteInput();
         } else if (v == remoteInputText) {
-            show();
+            show(v, false);
+        }
+    }
+
+    private void sendRemoteInput() {
+        if (remoteInputText.getText().length() == 0) return;
+
+        setWindowManagerFocus(false);
+        remoteInputText.setFocusable(false);
+        remoteInputText.setEnabled(false);
+        sendButton.setVisibility(INVISIBLE);
+        sendProgress.setVisibility(VISIBLE);
+
+        Bundle resultData = new Bundle();
+        resultData.putString(remoteInput.getResultKey(), remoteInputText.getText().toString());
+        Intent result = new Intent().addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+        RemoteInput.addResultsToIntent(remoteInputs, result, resultData);
+        try {
+            pendingIntent.send(getContext(), 0, result);
+        } catch (PendingIntent.CanceledException e) {
+            e.printStackTrace();
         }
     }
 
@@ -162,5 +191,39 @@ public class RemoteInputHelperView extends FrameLayout implements View.OnClickLi
         if (view == remoteInputText) {
             setWindowManagerFocus(hasFocus);
         }
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+        if (event == null
+                && (actionId == EditorInfo.IME_ACTION_DONE
+                || actionId == EditorInfo.IME_ACTION_NEXT
+                || actionId == EditorInfo.IME_ACTION_SEND)) {
+            if (remoteInputText.length() > 0) {
+                sendRemoteInput();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private void updateSendButton() {
+        sendButton.setEnabled(remoteInputText.getText().length() != 0);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        updateSendButton();
     }
 }
