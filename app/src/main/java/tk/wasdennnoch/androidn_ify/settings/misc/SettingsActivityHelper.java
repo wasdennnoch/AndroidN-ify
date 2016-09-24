88c9de84ff1da254c1ce3851b1047e96e8954189
paphonbth <paphonbth@users.noreply.github.com>
2016-09-24 14:39:59 +0700

    [EXPERIMENTAL] Settings drawer (#948)
    * Settings drawer
    
    * Use RecyclerView for settings drawer

---
+++ app/src/main/java/tk/wasdennnoch/androidn_ify/settings/misc/SettingsActivityHelper.java
@@ -0,0 +1,222 @@
+package tk.wasdennnoch.androidn_ify.settings.misc;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.Toolbar;
+
+import com.android.internal.view.menu.ActionMenuItem;
+
+import java.util.List;
+
+import de.robv.android.xposed.XposedHelpers;
+import tk.wasdennnoch.androidn_ify.R;
+import tk.wasdennnoch.androidn_ify.XposedHook;
+import tk.wasdennnoch.androidn_ify.utils.ResourceUtils;
+
+public class SettingsActivityHelper implements View.OnClickListener {
+
+    private static final String TAG = "SettingsActivityHelper";
+    private static final String CLASS_UTILS = "com.android.settings.Utils";
+
+    private static List<Object> sDashboardCategories;
+    private static Class<?> sClassUtils;
+    private ActionMenuItem mNavItem;
+    private boolean mIsShowingDashboard;
+
+    private Activity mActivity;
+    private DrawerLayout mDrawerLayout;
+    private SettingsDrawerAdapter mDrawerAdapter;
+
+    private int mFailCount = 0;
+    private Runnable mUpdateCategories = new Runnable() {
+        @Override
+        public void run() {
+            new CategoriesUpdater().execute();
+        }
+    };
+
+    @SuppressWarnings("ConstantConditions")
+    public SettingsActivityHelper(Activity activity) {
+        mActivity = activity;
+        if (sClassUtils == null) {
+            sClassUtils = XposedHelpers.findClass(CLASS_UTILS, mActivity.getClassLoader());
+        }
+        LayoutInflater inflater = LayoutInflater.from(activity);
+        inflater.setFactory2(new LayoutInflater.Factory2() {
+            @Override
+            public View onCreateView(String name, Context context, AttributeSet attrs) {
+                if (name.equals(DrawerLayout.class.getCanonicalName())) {
+                    return new DrawerLayout(context, attrs);
+                } else if (name.equals(RecyclerView.class.getCanonicalName())) {
+                    return new RecyclerView(context, attrs);
+                } else return null;
+            }
+
+            @Override
+            public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+                return onCreateView(name, context, attrs);
+            }
+        });
+        mIsShowingDashboard = XposedHelpers.getBooleanField(activity, "mIsShowingDashboard");
+        boolean mDisplayHomeAsUpEnabled = XposedHelpers.getBooleanField(activity, "mDisplayHomeAsUpEnabled");
+        ViewGroup content = (ViewGroup) XposedHelpers.getObjectField(activity, "mContent");
+        mDrawerLayout = (DrawerLayout) inflater.inflate(ResourceUtils.getInstance(activity).getLayout(R.layout.settings_with_drawer), content, false);
+        Toolbar toolbar = (Toolbar) mDrawerLayout.findViewById(R.id.action_bar);
+        activity.setActionBar(toolbar);
+        ActionBar actionBar = activity.getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(mDisplayHomeAsUpEnabled);
+        actionBar.setHomeButtonEnabled(mDisplayHomeAsUpEnabled);
+        toolbar.setNavigationOnClickListener(this);
+        mNavItem = new ActionMenuItem(activity,
+                0, android.R.id.home, 0, 0, "");
+        if (!mIsShowingDashboard) {
+            actionBar.setHomeAsUpIndicator(ResourceUtils.getInstance(activity).getDrawable(R.drawable.ic_menu));
+            content = (ViewGroup) content.getParent().getParent();
+        }
+        ViewGroup parent = (ViewGroup) content.getParent();
+        int index = parent.indexOfChild(content);
+        parent.removeView(content);
+        ((FrameLayout) mDrawerLayout.findViewById(R.id.content_frame)).addView(content);
+        parent.addView(mDrawerLayout, index);
+        mDrawerAdapter = new SettingsDrawerAdapter(activity);
+        mDrawerAdapter.setSettingsActivityHelper(this);
+        RecyclerView recyclerView = (RecyclerView) activity.findViewById(R.id.left_drawer);
+        recyclerView.setLayoutManager(new LinearLayoutManager(mActivity));
+        recyclerView.setAdapter(mDrawerAdapter);
+        new CategoriesUpdater().execute();
+    }
+
+    protected void onCategoriesChanged() {
+        if (sDashboardCategories != null) {
+            updateDrawer();
+        } else if (mFailCount < 10) {
+            mFailCount++;
+            mDrawerLayout.postDelayed(mUpdateCategories, 1000);
+        }
+    }
+
+    public void updateDrawer() {
+        if (mDrawerLayout == null) {
+            return;
+        }
+        mDrawerAdapter.updateCategories();
+    }
+
+    public void updateDrawerLock() {
+        if (mDrawerAdapter.getItemCount() != 0) {
+            mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+        } else {
+            mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+        }
+    }
+
+    public void openDrawer() {
+        if (mDrawerLayout != null) {
+            mDrawerLayout.openDrawer(GravityCompat.START);
+        }
+    }
+
+    public void closeDrawer() {
+        if (mDrawerLayout != null) {
+            mDrawerLayout.closeDrawers();
+        }
+    }
+
+    public boolean openTile(Object tile) {
+        closeDrawer();
+        if (tile == null) {
+            mActivity.startActivity(new Intent(Settings.ACTION_SETTINGS).addFlags(
+                    Intent.FLAG_ACTIVITY_CLEAR_TASK));
+            return true;
+        }
+        if (openFragment(tile)) {
+            return false;
+        }
+        try {
+            // Show menu on top level items.
+            Intent intent = (Intent) XposedHelpers.getObjectField(tile, "intent");
+            //tile.intent.putExtra(EXTRA_SHOW_MENU, true);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            mActivity.startActivity(intent);
+            //}
+        } catch (ActivityNotFoundException e) {
+            Intent intent = (Intent) XposedHelpers.getObjectField(tile, "intent");
+            XposedHook.logE(TAG, "Couldn't find tile " + intent, e);
+        }
+        return true;
+    }
+
+    private boolean openFragment(Object tile) {
+        String fragment = (String) XposedHelpers.getObjectField(tile, "fragment");
+        if (fragment != null) {
+            Bundle fragmentArguments = (Bundle) XposedHelpers.getObjectField(tile, "fragmentArguments");
+            int titleRes = XposedHelpers.getIntField(tile, "titleRes");
+            CharSequence title = (CharSequence) XposedHelpers.callMethod(tile, "getTitle", mActivity.getResources());
+            XposedHelpers.callStaticMethod(sClassUtils, "startWithFragment", new Class[] {Context.class, String.class, Bundle.class, Fragment.class, int.class, int.class, CharSequence.class},
+                    mActivity, fragment, fragmentArguments, null, 0, titleRes, title);
+            return true;
+        }
+        return false;
+    }
+
+    protected void onTileClicked(Object tile) {
+        if (openTile(tile)) {
+            mActivity.finish();
+        }
+    }
+
+    public static List<Object> getDashboardCategories() {
+        return sDashboardCategories;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (!mIsShowingDashboard && mDrawerLayout != null
+                && mDrawerAdapter.getItemCount() != 0) {
+            openDrawer();
+        } else {
+            mActivity.onOptionsItemSelected(mNavItem);
+        }
+    }
+
+    private class CategoriesUpdater extends AsyncTask<Void, Void, List<Object>> {
+        @SuppressWarnings("unchecked")
+        @Override
+        protected List<Object> doInBackground(Void... params) {
+            try {
+                if (sDashboardCategories != null) {
+                    return sDashboardCategories;
+                }
+                return (List<Object>) XposedHelpers.callMethod(mActivity, "getDashboardCategories", false);
+            } catch (Throwable t) {
+                XposedHook.logE(TAG, "Can't update categories", t);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(List<Object> dashboardCategories) {
+            sDashboardCategories = dashboardCategories;
+            onCategoriesChanged();
+        }
+    }
+}
---
+++ app/src/main/java/tk/wasdennnoch/androidn_ify/settings/misc/SettingsDrawerAdapter.java
@@ -0,0 +1,220 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tk.wasdennnoch.androidn_ify.settings.misc;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.robv.android.xposed.XposedHelpers;
+import tk.wasdennnoch.androidn_ify.R;
+import tk.wasdennnoch.androidn_ify.XposedHook;
+import tk.wasdennnoch.androidn_ify.utils.ResourceUtils;
+
+public class SettingsDrawerAdapter extends RecyclerView.Adapter<SettingsDrawerAdapter.DrawerViewHolder> {
+
+    private static final String TAG = "SettingsDrawerAdapter";
+    private static final int TYPE_SPACER = 0;
+    private static final int TYPE_CATEGORY = 1;
+    private static final int TYPE_TILE = 2;
+    private final ArrayList<Item> mItems = new ArrayList<>();
+    private final Activity mActivity;
+    private SettingsActivityHelper mHelper;
+
+    public SettingsDrawerAdapter(Activity activity) {
+        mActivity = activity;
+    }
+
+    void updateCategories() {
+        new CategoriesUpdater().execute();
+    }
+
+    @SuppressWarnings("unchecked")
+    private void updateCategoriesInternal() {
+        ResourceUtils res = ResourceUtils.getInstance(mActivity);
+        List<Object> categories = SettingsActivityHelper.getDashboardCategories();
+        if (categories == null) return;
+        mItems.clear();
+        // Spacer.
+        mItems.add(null);
+        Item tile = new Item();
+        tile.label = res.getString(R.string.home);
+        tile.icon = res.getDrawable(R.drawable.home);
+        mItems.add(tile);
+        for (int i = 0; i < categories.size(); i++) {
+            Item category = new Item();
+            category.icon = null;
+            Object dashboardCategory = categories.get(i);
+            category.label = (CharSequence) XposedHelpers.callMethod(dashboardCategory, "getTitle", mActivity.getResources());
+            mItems.add(category);
+            List<Object> tiles = (List<Object>) XposedHelpers.getObjectField(dashboardCategory, "tiles");
+            for (int j = 0; j < tiles.size(); j++) {
+                tile = new Item();
+                Object dashboardTile = tiles.get(j);
+                tile.label = (CharSequence) XposedHelpers.callMethod(dashboardTile, "getTitle", mActivity.getResources());
+                tile.icon = getIcon(dashboardTile);
+                tile.tile = dashboardTile;
+                mItems.add(tile);
+            }
+        }
+    }
+
+    private Drawable getIcon(Object dashboardTile) {
+        int iconRes = XposedHelpers.getIntField(dashboardTile, "iconRes");
+        String iconPkg = (String) XposedHelpers.getObjectField(dashboardTile, "iconPkg");
+        if (!TextUtils.isEmpty(iconPkg)) {
+            try {
+                Drawable drawable = mActivity.getPackageManager()
+                        .getResourcesForApplication(iconPkg).getDrawable(iconRes, null);
+                if (!iconPkg.equals(mActivity.getPackageName()) && drawable != null) {
+                    // If this drawable is coming from outside Settings, tint it to match the color.
+                    TypedValue tintColorValue = new TypedValue();
+                    mActivity.getResources().getValue(mActivity.getResources().getIdentifier("external_tile_icon_tint_color", "color", mActivity.getPackageName()),
+                            tintColorValue, true);
+                    // If tintColorValue is TYPE_ATTRIBUTE, resolve it
+                    if (tintColorValue.type == TypedValue.TYPE_ATTRIBUTE) {
+                        mActivity.getTheme().resolveAttribute(tintColorValue.data,
+                                tintColorValue, true);
+                    }
+                    drawable.setTintMode(android.graphics.PorterDuff.Mode.SRC_ATOP);
+                    drawable.setTint(tintColorValue.data);
+                }
+                return drawable;
+            } catch (Throwable t) {
+                XposedHook.logE(TAG, "Can't get icon for tile", t);
+                return null;
+            }
+        } else if (iconRes > 0) {
+            return mActivity.getDrawable(iconRes);
+        } else {
+            return null;
+        }
+    }
+
+    public Object getTile(int position) {
+        return mItems.get(position) != null ? mItems.get(position).tile : null;
+    }
+
+    @Override
+    public DrawerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView;
+        switch (viewType) {
+            case TYPE_SPACER:
+                itemView = inflate(mActivity, R.layout.drawer_spacer, parent, false);
+                itemView.setEnabled(false);
+                break;
+            case TYPE_CATEGORY:
+                itemView = inflate(mActivity, R.layout.drawer_category, parent, false);
+                itemView.setEnabled(false);
+                break;
+            default:
+                itemView = inflate(mActivity, R.layout.drawer_item, parent, false);
+        }
+        return new DrawerViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(DrawerViewHolder holder, int position) {
+        int type = getItemViewType(position);
+        if (type == TYPE_SPACER) return;
+        ViewGroup itemView = (ViewGroup) holder.getItemView();
+        Item item = mItems.get(position);
+        if (type == TYPE_TILE) {
+            ((ImageView) itemView.findViewById(android.R.id.icon)).setImageDrawable(item.icon);
+        }
+        ((TextView) itemView.findViewById(android.R.id.title)).setText(item.label);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Item item = mItems.get(position);
+        if (item == null) {
+            return TYPE_SPACER;
+        } else if (item.icon == null) {
+            return TYPE_CATEGORY;
+        } else {
+            return TYPE_TILE;
+        }
+    }
+
+    private View inflate(Context context, int resource, ViewGroup root, boolean attachToRoot) {
+        return LayoutInflater.from(context).inflate(ResourceUtils.getInstance(context).getLayout(resource), root, attachToRoot);
+    }
+
+    public void setSettingsActivityHelper(SettingsActivityHelper settingsActivityHelper) {
+        mHelper = settingsActivityHelper;
+    }
+
+    private static class Item {
+        public Drawable icon;
+        public CharSequence label;
+        public Object tile;
+    }
+
+    private class CategoriesUpdater extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            updateCategoriesInternal();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            notifyDataSetChanged();
+            if (mHelper != null) mHelper.updateDrawerLock();
+        }
+    }
+
+    public class DrawerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public DrawerViewHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+        }
+
+        public View getItemView() {
+            return itemView;
+        }
+
+        @Override
+        public void onClick(View v) {
+            mHelper.onTileClicked(mItems.get(getAdapterPosition()).tile);
+        }
+    }
+}
